738a737c72c8b06ea28ee2231570003e172f4d9f
Nosagie <prognisiv@gmali.com>
2022-01-07 20:35:13 -0500

    first commit

---
+++ benin city vsc/scripts/formScript.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBC5kcnG_EwBFOWU5elhw_7BuGG3xeScCY",
+    authDomain: "covid-19-website-3ed23.firebaseapp.com",
+    databaseURL: "https://covid-19-website-3ed23-default-rtdb.firebaseio.com",
+    projectId: "covid-19-website-3ed23",
+    storageBucket: "covid-19-website-3ed23.appspot.com",
+    messagingSenderId: "956423135061",
+    appId: "1:956423135061:web:e2c971850793b2c1ed59dc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
